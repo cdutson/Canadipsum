6734c462f9f8bbef76eb9f02b216a25e759cb1f0
corey <c.dutson@ry.com>
2013-07-17 15:18:17 +0100

    style tweaks

--- static/js/general.js
+++ static/js/general.js
@@ -1,5 +1,6 @@
 
 //Yeah I ended up using jquery cause LAZY.
 $(document).ready(function(){
+	document.getElementsByTagName('html')[0].className += " js";
 	$.backstretch('/static/img/bg/'+document.getElementsByTagName('html')[0].getAttribute('data-bg'));
 });
